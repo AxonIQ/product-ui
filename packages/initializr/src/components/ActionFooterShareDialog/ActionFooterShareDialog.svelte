d51c3dd08b4c2f6cc284c4e63c9afba9a375a005
Dusan Perkovic <dusan.perkovic@axoniq.io>
2021-07-05 16:07:38 +0200

    [initializr]: Update share dialog with title slot.

--- packages/initializr/src/components/ActionFooterShareDialog/ActionFooterShareDialog.svelte
+++ packages/initializr/src/components/ActionFooterShareDialog/ActionFooterShareDialog.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-
-    
     import { userSelection } from "../../userSelectionStore";
     import { Button, Dialog, Input, Typography } from '@axoniq-product-ui/core';
     
@@ -12,25 +10,20 @@
         open={visible}
         onClose={() => visible = false}
     >
-        <div class="action-footer-share-dialog">
-            <Typography size="xl" weight="bold">Share your configuration</Typography>
-            <Typography size="s">Use this link to share the current configuration. Attributes can be removed from the URL if you want to rely on our defaults.</Typography>
-            <div class="action-footer-share-dialog__input">
-                <Input
-                    value={shareableLink} />
-                <Button text="Copy" onClick={() => {
-                    navigator.clipboard.writeText(shareableLink);
-                }}/>
-            </div>
-        </div>
+    <div slot="title">
+        <Typography size="xl" weight="bold">Share your configuration</Typography>
+    </div>
+    <Typography size="s">Use this link to share the current configuration. Attributes can be removed from the URL if you want to rely on our defaults.</Typography>
+    <div class="action-footer-share-dialog__input">
+        <Input
+            value={shareableLink} />
+        <Button text="Copy" onClick={() => {
+            navigator.clipboard.writeText(shareableLink);
+        }}/>
+    </div>
     </Dialog>
     
 <style lang="scss">
-    .action-footer-share-dialog {
-        display: flex;
-        flex-direction: column;
-        gap: 24px;
-    }
     .action-footer-share-dialog__input {
         display: flex;
         gap: 10px;
