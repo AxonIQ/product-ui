4debc3bb6c8c7f2dc6ea33030833400f72c941e1
Dusan Perkovic <dusan.perkovic@axoniq.io>
2021-07-26 17:59:25 +0200

    Add link to core pacakage and style guide.

---
+++ packages/core/src/components/Link/Link.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Typography from "../Typography/Typography.svelte";
+    
+        export let href = '';
+        export let target = '';
+    </script>
+    
+    <a
+        class="link"
+        {href}
+        {target}>
+        <Typography
+            center
+            color="rhino"
+            size="s">
+                <slot />
+        </Typography>
+    </a>
+    
+    <style lang="scss">
+        @use "../../../stylesheets/colors.scss";
+    
+        .link {
+            text-decoration: none;
+            &:hover {
+                text-decoration: underline;
+                text-decoration-color: colors.$elephant;
+                text-decoration-thickness: 1px;
+                text-underline-offset: 7px;
+            }
+        }
+    </style>
