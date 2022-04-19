142f59fab1746847a663d020325d48bd61f45ee6
Dusan Perkovic <pljeskavicica@gmail.com>
2022-04-19 15:52:12 +0200

    Add focus state.

--- packages/core/src/components/Radio/Radio.svelte
+++ packages/core/src/components/Radio/Radio.svelte
@@ -8,7 +8,7 @@
     <label class="flex items-center cursor-pointer">
         <input
             type="radio"
-            class="hidden"
+            class="opacity-0 w-0 h-0 m-0"
             {value}
             {name}
             bind:group={group}
@@ -19,9 +19,13 @@
 
 <style lang="scss">
     .design {
-        @apply w-2 h-2 rounded-full outline-offset-2 outline outline-2 outline-ox mr-5;
+        @apply w-2 h-2 rounded-full outline-offset-2 outline outline-2 outline-ox mr-3;
     }
     input[type="radio"]:checked~.design {
         @apply outline-ox bg-ox;
+    }
+    input[type="radio"]:focus-visible~.design {
+        @apply shadow-ox;
+        box-shadow: 0 0 0 7px #fff, 0 0 4px 8px;
     }
 </style>
