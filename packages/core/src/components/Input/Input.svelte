387cafbc66dfd2c4adc162242cbde55943913c31
Dusan Perkovic <pljeskavicica@gmail.com>
2022-04-28 16:08:55 +0200

    Update Input to show labels at top.

--- packages/core/src/components/Input/Input.svelte
+++ packages/core/src/components/Input/Input.svelte
@@ -2,16 +2,22 @@
     export let placeholder = '';
     export let value = '';
     export let disabled = false;
+    export let label;
 </script>
-<input
-    class="input"
-    disabled={disabled}
-    placeholder={placeholder}
-    bind:value={value}
-/>
+<label class="relative">
+    <input
+        class="input"
+        disabled={disabled}
+        placeholder={placeholder}
+        bind:value={value}
+    />
+    {#if label}
+        <span class="absolute -top-2 left-1 text-sm bg-dove px-1 leading-none">{label}</span>
+    {/if}
+</label>
 <style lang="scss">
     .input {
-        @apply border border-solid border-elephant py-1 px-2 focus:outline-none text-sm w-full;
+        @apply border border-solid border-elephant py-1 px-2 focus:outline-none text-sm w-full py-2;
         box-shadow: inset 0px 1px 2px rgba(0, 0, 0, 0.25);
     }
 </style>
