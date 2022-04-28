4aa7d79a47b01b3c14ffbd8e51ec90ada6d94d68
Dusan Perkovic <pljeskavicica@gmail.com>
2022-04-28 18:24:23 +0200

    Add shadow to dropdown. Replace Radio with dropdown in initializr.

--- packages/core/src/components/DropdownItems/DropdownItems.svelte
+++ packages/core/src/components/DropdownItems/DropdownItems.svelte
@@ -2,6 +2,11 @@
   export let value: string;
 </script>
 
-<select bind:value class="bg-dove w-full cursor-pointer border border-solid border-elephant py-1 px-2 focus:outline-none text-sm">
+<select bind:value class="select min-h-[42px] bg-dove w-full cursor-pointer border border-solid border-elephant py-2 px-2 focus:outline-none text-sm">
   <slot />
 </select>
+<style lang="scss">
+  .select {
+    box-shadow: inset 0px 1px 2px rgba(0, 0, 0, 0.25);
+  }
+</style>
--- packages/initializr/src/components/HomeSettings/HomeSettings.svelte
+++ packages/initializr/src/components/HomeSettings/HomeSettings.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { Card,Radio } from '@axoniq-product-ui/core';
+import { Radio, Dropdown, DropdownItems, DropdownLabel, DropdownItem } from '@axoniq-product-ui/core';
 import type { DataType,SettingsItem } from "../../dataTypes";
 
     export let projectTypeData: DataType;
@@ -24,60 +24,40 @@ import type { DataType,SettingsItem } from "../../dataTypes";
     <div class="flex items-center mb-2 h-12">
         <div class="text-xl font-bold">Settings</div>
     </div>
-    <div class="grid grid-cols-[auto_1fr] grid-flow-row gap-6 justify-center">
-        
-        <div class="text-sm">Project</div>
-        <div class="flex flex-col gap-2">
-            {#each projectTypeData.values as projectTypeItem (projectTypeItem.id) }
-                {#if projectTypeItem.tags.format === "project"}
-                    <Radio
-                        value={projectTypeItem.id}
-                        name="projectType"
-                        bind:group={projectTypeSelected}
-                    >
-                        <div class="text-sm">{projectTypeItem.name}</div>
-                    </Radio>
-                {/if}
-            {/each}
-        </div>
-
-        <div class="text-sm">Language</div>
-        <div class="flex flex-col gap-2">
-            {#each projectLanguageData.values as projectLanguageItem (projectLanguageItem.id) }
-                <Radio
-                    value={projectLanguageItem.id}
-                    name="projectLanguage"
-                    bind:group={projectLanguageSelected}
-                >
-                    <div class="text-sm">{projectLanguageItem.name}</div>
-                </Radio>
-            {/each}
-        </div>
-
-        <div class="text-sm">Java</div>
-        <div class="flex flex-col gap-2">
-            {#each projectJavaVersionData.values as projectJavaVersionItem (projectJavaVersionItem.id) }
-                <Radio
-                    value={projectJavaVersionItem.id}
-                    name="projectJavaVersion"
-                    bind:group={projectJavaVersionSelected}
-                >
-                    <div class="text-sm">{projectJavaVersionItem.name}</div>
-                </Radio>
-            {/each}
-        </div>
-
-        <div class="text-sm">Packaging</div>
-        <div class="flex flex-col gap-2">
-            {#each projectPackagingData.values as projectPackagingItem (projectPackagingItem.id) }
-                <Radio
-                    value={projectPackagingItem.id}
-                    name="projectPackaging"
-                    bind:group={projectPackagingSelected}
-                >
-                    <div class="text-sm">{projectPackagingItem.name}</div>
-                </Radio>
-            {/each}
-        </div>
+    <div class="flex flex-col gap-6 justify-center">
+        <Dropdown>
+            <DropdownLabel>Project</DropdownLabel>
+            <DropdownItems bind:value={projectTypeSelected}>
+                {#each projectTypeData.values as projectTypeItem (projectTypeItem.id) }
+                    {#if projectTypeItem.tags.format === "project"}
+                        <DropdownItem value={projectTypeItem.id}>{projectTypeItem.name}</DropdownItem>
+                    {/if}
+                {/each}
+            </DropdownItems>
+        </Dropdown>
+        <Dropdown>
+            <DropdownLabel>Language</DropdownLabel>
+            <DropdownItems bind:value={projectLanguageSelected}>
+                {#each projectLanguageData.values as projectLanguageItem (projectLanguageItem.id) }
+                    <DropdownItem value={projectLanguageItem.id}>{projectLanguageItem.name}</DropdownItem>
+                {/each}
+            </DropdownItems>
+        </Dropdown>
+        <Dropdown>
+            <DropdownLabel>Java</DropdownLabel>
+            <DropdownItems bind:value={projectJavaVersionSelected}>
+                {#each projectJavaVersionData.values as projectJavaVersionItem (projectJavaVersionItem.id) }
+                    <DropdownItem value={projectJavaVersionItem.id}>{projectJavaVersionItem.name}</DropdownItem>
+                {/each}
+            </DropdownItems>
+        </Dropdown>
+        <Dropdown>
+            <DropdownLabel>Packaging</DropdownLabel>
+            <DropdownItems bind:value={projectPackagingSelected}>
+                {#each projectPackagingData.values as projectPackagingItem (projectPackagingItem.id) }
+                    <DropdownItem value={projectPackagingItem.id}>{projectPackagingItem.name}</DropdownItem>
+                {/each}
+            </DropdownItems>
+        </Dropdown>
     </div>
 </div>
