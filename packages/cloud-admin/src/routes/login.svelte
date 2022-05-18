47ee1b415e3b22e844619382afe8c52eea27dcd6
Dusan Perkovic <pljeskavicica@gmail.com>
2022-05-18 23:05:42 +0200

    Finalize styling login page

--- packages/cloud-admin/src/routes/login.svelte
+++ packages/cloud-admin/src/routes/login.svelte
@@ -5,12 +5,11 @@ import IconGoogle from "src/components/IconGoogle.svelte";
 import IconGithub from "src/components/IconGithub.svelte";
 
 </script>
-<div class="max-w-[1440px] mx-auto h-screen">
+<div class="max-w-6xl px-5 mx-auto h-screen">
   <div class="h-full flex justify-center items-center gap-10">
     <Logo />
     <div class="flex flex-col gap-8 w-full">
       <h1 class="text-2xl font-light">Welcome to AxonIQ Cloud</h1>
-      <hr class="w-full border-0 m-0 h-px bg-elephant"/>
       <div class="flex gap-4">
         <Button variant="secondary">
           <span class="font-black flex gap-2 items-center"><IconGoogle/> Sign in with Google</span>
@@ -19,6 +18,7 @@ import IconGithub from "src/components/IconGithub.svelte";
           <span class="font-black flex gap-2 items-center"><IconGithub/> Sign in with Github</span>
         </Button>
       </div>
+      <hr class="w-full border-0 m-0 h-px bg-elephant"/>
       <div>
         No account yet? <Link href="#">Create account</Link>
       </div>
