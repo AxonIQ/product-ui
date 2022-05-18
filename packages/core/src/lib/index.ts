783b7d20abd0936f9aae592bfbc0f02bf1345df6
Dusan Perkovic <pljeskavicica@gmail.com>
2022-05-18 04:10:20 +0200

    Fix svelte reference. Fix index imports.

--- packages/core/src/lib/index.ts
+++ packages/core/src/lib/index.ts
@@ -1,13 +1,29 @@
-export * from './Button';
-export * from './Card';
-export * from './Dialog';
-export * from './IconButton';
-export * from './Input';
-export * from './Radio';
-export * from './Toggle';
-export * from './Search';
-export * from './Link';
-export * from './DropdownItem';
-export * from './DropdownItems';
-export * from './Dropdown';
-export * from './DropdownLabel';
+import Button from './Button.svelte';
+import Card from './Card.svelte';
+import Dialog from './Dialog.svelte';
+import IconButton from './IconButton.svelte';
+import Input from './Input.svelte';
+import Radio from './Radio.svelte';
+import Toggle from './Toggle.svelte';
+import Search from './Search.svelte';
+import Link from './Link.svelte';
+import DropdownItem from './DropdownItem.svelte';
+import DropdownItems from './DropdownItems.svelte';
+import Dropdown from './Dropdown.svelte';
+import DropdownLabel from './DropdownLabel.svelte';
+
+export { 
+  Button,
+  Card,
+  Dialog,
+  IconButton,
+  Input,
+  Radio,
+  Toggle,
+  Search,
+  Link,
+  DropdownItem,
+  DropdownItems,
+  Dropdown,
+  DropdownLabel,
+}
