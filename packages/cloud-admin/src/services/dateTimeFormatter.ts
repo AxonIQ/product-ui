13ced8b8e402dc84f2e7fab4857238e0e58e0b5b
Dusan Perkovic <pljeskavicica@gmail.com>
2022-06-20 14:07:17 +0200

    Update date format import

--- packages/cloud-admin/src/services/dateTimeFormatter.ts
+++ packages/cloud-admin/src/services/dateTimeFormatter.ts
@@ -1,4 +1,4 @@
-import format from "date-fns/format";
+import format from "date-fns/format/index.js";
 
 export const formatDateTime = (dateTime: string) => {
     return format(new Date(dateTime), "dd/MM/yyyy HH:mm");
